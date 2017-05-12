92557b163d4c96c8cc9e2dc80356d3edc7974135
koeshiro <koeshiro@outlook.com>
2017-05-13 00:31:28 +0300

    Переосмыслен формат обработки шаблона
    Полностью переделана обработка шаблона

--- view.js
+++ view.js
@@ -1,15 +1,51 @@
-﻿(function(){
-	/* ERRORS Numbers
-	* 1. Not valid var name
-	*/
+﻿(function(window){
+	/* Just some new templater */
 	var _string    = "string";
 	var _object    = "object";
 	var _array 	   = "array";
 	var _function  = "function";
+	//This code taken from microtemplate created by Paul Miller
+	var entityRe = new RegExp('[&<>"\']', 'g');
+	var escapeExpr = function(string) {
+		if (string == null) return '';
+		return ('' + string).replace(entityRe, function(match) {
+			return htmlEntities[match];
+		});
+	};
+	var settings = {
+		evaluate    : /<%([\s\S]+?)%>/g,
+		interpolate : /<%=([\s\S]+?)%>/g,
+		escape      : /<%-([\s\S]+?)%>/g
+	};
+	var matcher = new RegExp([
+		(settings.escape || noMatch).source,
+		(settings.interpolate || noMatch).source,
+		(settings.evaluate || noMatch).source
+	].join('|') + '|$', 'g');
+	var noMatch = /(.)^/;
+	var escapes = {
+		"'":      "'",
+		'\\':     '\\',
+		'\r':     'r',
+		'\n':     'n',
+		'\t':     't',
+		'\u2028': 'u2028',
+		'\u2029': 'u2029'
+	};
+
+	var escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g;
+	var htmlEntities = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#x27;'
+	};
+	//End
 	var helper={};
 	helper.ObjectLength=function(o){
 	    var s = 0, k;
-	    for (k in o) {
+	    for(k in o){
 	        if (o.hasOwnProperty(k)) s++;
 	    }
 	    return s;
@@ -30,50 +66,83 @@
 		else return false;
 	}
 	//Templater
-	Templater=function (template,object,context){
-		this.Temp=this.render(template,object); this.raw=template; this.context=context;
+	/*
+	* var context - used when calling the generated template function
+	* var CacheTemp - last rendered code
+	* var RAWTEMP - RAW template code
+	* var DataCache - last used data
+	*/
+	Templater=function(template,object,context){
+		this.DataCache=object||{},this.RAWTEMP=template||''; this.context=context||this;
 		return this;
 	}
-	Templater.prototype.render=function(t,o){
-		var r=new RegExp("<@[\\S\\s]+?(execute\\(\\{\\s{0,}@>[\\S\\s]+?(@>))|<@([\\S\\s]+?(@>))","mg"), HTML=t,t=this;
-			HTML=HTML.replace(r,function(s){
-				var script=s.replace('<@','').replace('@>','');
-				var html=t.execute(script,o);
-				return html;
-			});
-		return HTML;
+
+	Templater.prototype.Render=function(){
+
+			this.CacheTemp=this.Execute(this.RAWTEMP,this.DataCache);
+			return this.CacheTemp;
+
 	}
-	Templater.prototype.rerender=function(o){
-		this.Temp=this.render(this.raw,o);
-		return this;
-	}
-	Templater.prototype.execute=function(s,o){
-		var script="(function(templater_data){ \n\
-			var templater_html_text_var=\"\"; \n\
-			var each=function(s){ \n\
-				templater_html_text_var+=s; \n\
+
+	Templater.prototype.Execute=function(tmp,obj){
+		var HTML=tmp,t=this,templater_data={},index = 0,script = "(function(__templater_data){ \n\
+			var __templater_html_text_var=\"\"; \n\
+			var __t,echo=function(){ \n\
+				__templater_html_text_var+=Array.prototype.join.call(arguments,''); \n\
 			} \n\
 			var execute=function(t,o){ \n\
 				return new Templater(t,o); \n\
-			} \n\
-			console.log(this);\
-			";
-		var templater_data={};
-		for(var k in o){
-			script+="var "+k+"=templater_data."+k+";";
-			templater_data[k]=o[k];
+			} \n";
+		//Add data to array for executing
+		for(var k in obj){
+			script+="var "+k+"=__templater_data."+k+";";
+			templater_data[k]=obj[k];
 		}
-		script+=s;
-		script+=" \n\
-		return templater_html_text_var; \n\
+		//end
+		script+="__templater_html_text_var+='";
+		//Render script.
+		HTML.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
+			script+=HTML.slice(index, offset).replace(escaper, function(match) { return '\\' + escapes[match]; });
+			if(escape){
+				script+="'+\n((__t=(" + escape + "))==null?'':escapeExpr(__t))+\n'";
+			}
+			if(interpolate){
+				script+="'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+			}
+			if(evaluate){
+				script+="';\n" + evaluate + "\n__templater_html_text_var+='";
+			}
+			index = offset + match.length;
+				return match;
+		});
+		script += "';\n\
+			return __templater_html_text_var; \n\
 		});"
 		var result=eval(script).call((this.context!=""?this.context:this),templater_data);
 		return result;
 	}
+
+	Templater.prototype.ReRender=function(o){
+		this.CacheTemp=this.Render(this.RAWTEMP,o);
+		return this.CacheTemp;
+	}
+	Templater.prototype.SetData=function(o){
+		this.DataCache=o;
+		return this;
+	}
+	Templater.prototype.SetTemp=function(s){
+		this.RAWTEMP=s;
+		return this;
+	}
+	Templater.prototype.SetContext=function(c){
+		this.context=c;
+		return this;
+	}
 	Templater.prototype.toString=function(){
-		return this.Temp;
+		return this.CacheTemp;
 	}
 	Templater.prototype.context="";
-	Templater.prototype.Temp="";
-	Templater.prototype.raw="";
-})()
+	Templater.prototype.CacheTemp="";
+	Templater.prototype.RAWTEMP="";
+	Templater.prototype.DataCache={};
+})(window);
